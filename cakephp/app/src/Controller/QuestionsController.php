4418b28197e967375b6397c293faa5e217126757
hm_nakatani_taro <nakatani.taro@hamee.co.jp>
2020-09-27 18:50:10 +0900

    0807

--- cakephp/app/src/Controller/QuestionsController.php
+++ cakephp/app/src/Controller/QuestionsController.php
@@ -11,6 +11,7 @@ class QuestionsController extends AppController {
      */
     public function initialize() {
         parent::initialize();
+        $this->loadModel('Answers');
     }
 
     /**
@@ -19,7 +20,7 @@ class QuestionsController extends AppController {
      * @return \Cake\Http\Response|void
      */
     public function index() {
-        $questions = $this->paginate($this->Questions->find(), [
+        $questions = $this->paginate($this->Questions->findQuestionsWithAnsweredCount(), [
             'order' => ['Questions.id' => 'DESC']
         ]);
 
@@ -47,5 +48,49 @@ class QuestionsController extends AppController {
         }
 
         $this->set(compact('question'));
+    }
+
+    /**
+     * 質問詳細画面
+     *
+     * @param int $id 質問ID
+     * @return void
+     */
+    public function view(int $id)
+    {
+        $question = $this->Questions->get($id);
+
+        $answers = $this
+            ->Answers
+            ->find()
+            ->where(['Answers.question_id' => $id])
+            ->orderAsc('Answers.id')
+            ->all();
+
+        $newAnswer = $this->Answers->newEntity();
+
+        $this->set(compact('question', 'answers', 'newAnswer'));
+    }
+
+    /**
+     * 質問削除処理
+     *
+     * @param int $id 質問ID
+     * @return \Cake\Http\Response|null 質問削除後に質問一覧画面へ遷移する
+     */
+    public function delete(int $id)
+    {
+        $this->request->allowMethod(['post']);
+
+        $question = $this->Questions->get($id);
+        // @TODO 質問を削除出来るのは質問投稿者のみとする
+
+        if ($this->Questions->delete($question)) {
+            $this->Flash->success('質問を削除しました');
+        } else {
+            $this->Flash->error('質問の削除に失敗しました');
+        }
+
+        return $this->redirect(['action' => 'index']);
     }
 }
