44336eab91961473e38ef3fa3d4aaea5b84b39f2
hm_nakatani_taro <nakatani.taro@hamee.co.jp>
2020-09-27 22:14:14 +0900

    8end

--- cakephp/app/src/Model/Table/AnswersTable.php
+++ cakephp/app/src/Model/Table/AnswersTable.php
@@ -2,6 +2,8 @@
 namespace App\Model\Table;
 
 use Cake\ORM\Table;
+use Cake\ORM\RulesChecker;
+use Cake\Validation\Validator;
 
 /**
  * Answers Model
@@ -38,35 +40,35 @@ class AnswersTable extends Table
      * @param \Cake\Validation\Validator $validator バリデーションインスタンス
      * @return \Cake\Validation\Validator バリデーションインスタンス
      */
-    // public function validationDefault(Validator $validator)
-    // {
-    //     $validator
-    //         ->nonNegativeInteger('id', 'IDが不正です')
-    //         ->allowEmpty('id', 'create', 'IDが不正です');
+    public function validationDefault(Validator $validator)
+    {
+        $validator
+            ->nonNegativeInteger('id', 'IDが不正です')
+            ->allowEmpty('id', 'create', 'IDが不正です');
 
-    //     $validator
-    //         ->scalar('body', '回答内容が不正です')
-    //         ->requirePresence('body', 'create', '回答内容が不正です')
-    //         ->notEmpty('body', '回答内容は必ず入力してください')
-    //         ->maxLength('body', 140, '回答内容は140字以内で入力してください');
+        $validator
+            ->scalar('body', '回答内容が不正です')
+            ->requirePresence('body', 'create', '回答内容が不正です')
+            ->notEmpty('body', '回答内容は必ず入力してください')
+            ->maxLength('body', 140, '回答内容は140字以内で入力してください');
 
-    //     return $validator;
-    // }
+        return $validator;
+    }
 
-    // /**
-    //  * ルールチェッカーを作成する
-    //  *
-    //  * @param \Cake\ORM\RulesChecker $rules ルールチェッカーのオブジェクト
-    //  * @return \Cake\ORM\RulesChecker ルールチェッカーのオブジェクト
-    //  */
-    // public function buildRules(RulesChecker $rules)
-    // {
-    //     $rules->add($rules->existsIn(
-    //         ['question_id'],
-    //         'Questions',
-    //         '質問が既に削除されているため回答することが出来ません'
-    //     ));
+    /**
+     * ルールチェッカーを作成する
+     *
+     * @param \Cake\ORM\RulesChecker $rules ルールチェッカーのオブジェクト
+     * @return \Cake\ORM\RulesChecker ルールチェッカーのオブジェクト
+     */
+    public function buildRules(RulesChecker $rules)
+    {
+        $rules->add($rules->existsIn(
+            ['question_id'],
+            'Questions',
+            '質問が既に削除されているため回答することが出来ません'
+        ));
 
-    //     return $rules;
-    // }
+        return $rules;
+    }
 }
